0450208ac70577253431572507e6a26c2ab9570a
rashidisalami <salam4life2270@yahoo.com>
2023-03-09 12:02:29 +0000

    homepage buttons

--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -2,13 +2,25 @@ import React from "react";
 import { useState, useEffect} from "react"; 
 import  ReactDOM  from "react-dom";
 import { Route, Router } from "react-router-dom";
+import './HomePage.scss';
+import { BiChevronRight} from "react-icons/bi";
 
 
 
 const HomePage = () => {
+
+
+
+
     return (
         <>
-          <h1>Hellooooooo</h1>
+          <div className="homepage">
+            <button >Add menu item<BiChevronRight/></button>
+            <button >Create order<BiChevronRight/></button>
+            <button >Shopping list<BiChevronRight/></button>
+            <button >Menu<BiChevronRight/></button>
+            <button >Contact us<BiChevronRight/></button>
+          </div>
         </>
     )
 }
