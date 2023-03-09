6dd61b870ad54b8da67093f562318f230b85b0fb
EvaVillanuevaHernandez <evaluna103@gmail.com>
2023-03-09 12:40:37 +0000

    delete unused folders

---
+++ frontend/src/pages/ProfilePage/ProfilePage.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import "./ProfilePage.scss";
+
+const ProfilePage = () => {
+    
+    return (
+        <>
+           <style>{'body {  background-color: #F4E9D6 }'}</style>
+           <img src="/images/logo.png" alt="background-logo" className="background-logo" />
+        </>
+    );
+};
+
+export default ProfilePage;
