50276b28ce0158b6f5591f9b1370b1c2ed93ab4a
EvaVillanuevaHernandez <evaluna103@gmail.com>
2023-03-09 13:54:23 +0000

    profile page

--- frontend/src/components/Theme.js
+++ frontend/src/components/Theme.js
@@ -1,3 +1,10 @@
+export const theme = {
+     colors: {
+          primary: "#E9C891",
+          secondary: "#F2B038",
+          thrtiary : "#6C310A",
+          quaternary: "#3A180A",
+          quinary: "#D06224"
 
      },
      fonts: ['Century, 24, Regular, Century, 22, Regular, Century, 20, Regular, Verdana, 16, Regular']
--- frontend/src/pages/ProfilePage/ProfilePage.js
+++ frontend/src/pages/ProfilePage/ProfilePage.js
@@ -2,11 +2,15 @@ import React, { useState } from "react";
 import "./ProfilePage.scss";
 
 const ProfilePage = () => {
-    
+
     return (
         <>
-           <style>{'body {  background-color: #F4E9D6 }'}</style>
-           <img src="/images/logo.png" alt="background-logo" className="background-logo" />
+            <style>{'body {  background-color: #F4E9D6 }'}</style>
+            <img src="/images/background.png" alt="background-logo" className="background-logo" />
+            <button className="close-button">x</button>
+            <img src="/images/profile.jpg" alt="Perfil" className="profile-image" />
+            <h1>Nombre de usuario</h1>
+            <button>Logout</button>
         </>
     );
 };
