1d18b796d587d59412ade09c831af8d0b1bf9270
EvaVillanuevaHernandez <evaluna103@gmail.com>
2023-03-10 10:10:34 +0000

    profile finish

--- frontend/src/pages/ProfilePage/ProfilePage.js
+++ frontend/src/pages/ProfilePage/ProfilePage.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./ProfilePage.scss";
-
+import { BiLogOut } from 'react-icons/bi';
 const ProfilePage = () => {
 
     return (
@@ -8,9 +8,19 @@ const ProfilePage = () => {
             <style>{'body {  background-color: #F4E9D6 }'}</style>
             <img src="/images/background.png" alt="background-logo" className="background-logo" />
             <button className="close-button">x</button>
-            <img src="/images/profile.jpg" alt="Perfil" className="profile-image" />
-            <h1>Nombre de usuario</h1>
-            <button>Logout</button>
+            <div class="imagen-container">
+                <img src="/images/profile.png" alt="Perfil" className="profile-image" />
+            </div>
+
+            <p className="username">Paco</p>
+            
+            <p className="settings">Settings</p>
+            
+
+            <div className="logout">
+                <BiLogOut className="i" />
+            </div>
+            
         </>
     );
 };
