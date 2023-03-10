9c67e980a332103caa4257d3640b14f27fb940c0
Rashidi <109029965+salam4life2270@users.noreply.github.com>
2023-03-10 11:07:06 +0000

    commit2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,19 @@ import CreateAccountPage from './pages/CreateAccountPage/CreateAccountPage';
 import { Route, Routes } from 'react-router-dom';
 import LoginPage from './pages/LoginPage/LoginPage';
 import ProfilePage from './pages/ProfilePage/ProfilePage';
+import HomePage from './pages/HomePage/HomePage';
+import ShoppingListPage from './pages/ShoppingListPage/ShoppingListPage';
+import CreateOrderPage from './pages/CreateOrderPage/CreateOrderPage';
 function App() {
   return (
     <>
     <Routes>
     <Route path="/" element={<LoginPage />} />
     <Route path="/CreateAccount" element={<CreateAccountPage />} />
-    <Route path="/ProfilePage" element={<ProfilePage />} />           
+    <Route path="/ProfilePage" element={<ProfilePage />} />  
+    <Route path="/HomePage" element={<HomePage />} />
+    <Route path="/CreateOrderPage" element={<CreateOrderPage />} />
+     <Route path="/ShoppingListPage" element={<ShoppingListPage />} />         
     </Routes>
 
     </>
