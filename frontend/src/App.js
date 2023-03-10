bc0da3ccfbd008709058bbc00c3b7a5dd02935ad
EvaVillanuevaHernandez <evaluna103@gmail.com>
2023-03-10 10:33:22 +0000

    add

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,15 @@ import CreateAccountPage from './pages/CreateAccountPage/CreateAccountPage';
 import { Route, Routes } from 'react-router-dom';
 import LoginPage from './pages/LoginPage/LoginPage';
 import ProfilePage from './pages/ProfilePage/ProfilePage';
+import AddMenuPage from './pages/AddMenuPage/AddmenuPage';
 function App() {
   return (
     <>
     <Routes>
     <Route path="/" element={<LoginPage />} />
     <Route path="/CreateAccount" element={<CreateAccountPage />} />
-    <Route path="/ProfilePage" element={<ProfilePage />} />           
+    <Route path="/ProfilePage" element={<ProfilePage />} />       
+    <Route path="/AddMenuPage" element={<AddMenuPage />} />       
     </Routes>
 
     </>
