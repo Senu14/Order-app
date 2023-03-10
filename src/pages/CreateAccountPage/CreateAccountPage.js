68355384b9744c81e53b5e05b293bfcbc4d2b2d8
Senu14 <101559338+Senu14@users.noreply.github.com>
2023-03-10 08:10:47 +0000

    Style changes

--- src/pages/CreateAccountPage/CreateAccountPage.js
+++ src/pages/CreateAccountPage/CreateAccountPage.js
@@ -1,24 +1,45 @@
 import React from 'react'
+import './CreateAccountPage.scss';
 
 const  CreateAccountPage = () => {
   return (
-     <>
-{/* //   <FormComponantStayle> */}
-    <h1>Create an Account</h1>
+<>
+<h1>Create an Account</h1>
+    <div class="container">
+      
 
-    <form action="" >
-    <label htmlFor="name">Name</label>
-    <input type="text"  />
-    <label htmlFor="email">Email</label>
-    <input type="email" />
-    <label htmlFor="password">Password</label>
-    <input type="password"name="password" />
-    <button type="text">Register</button>
-  </form>
-  {/* </FormComponantStayle> */}
-  </>
+    <form>
+
+    {/* <!-- Fulde Navn -->
+    <!-- An element kan have more than one class, now we have two class -->
+    <!-- always the right side class take place, the one you write last --> */}
+       <div class="form-control "> 
+            <label for="firstname">Name</label>
+            <input type="text" name="firstname" id="firstname" placeholder="write your name" class="inp-error"></input> 
+             
+        </div>
+
+        <div class="form-control">
+                <label for="email">Email</label>
+                <input type="email" name="email" id="email" placeholder="Skriv din email"/>
+                {/* <span class="email-error">email-error</span>  */}
+            </div>
+
+        <div>
+        <button type="button" onclick="validateForm(this.form)">Register</button>
+        </div>
+
+    </form>
+    
+</div>
+</>
+
 
   )
-}
 
-export default CreateAccountPage
+  }
+
+  
+  
+
+export default CreateAccountPage;
